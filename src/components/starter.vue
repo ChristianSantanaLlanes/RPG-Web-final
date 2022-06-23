4fc6889d595313499e99f07dbfa4b726dd84a3dd
ChristianSantanaLlanes <82761124+ChristianSantanaLlanes@users.noreply.github.com>
2022-06-23 10:23:20 -0400

    update starter.vue

--- src/components/starter.vue
+++ src/components/starter.vue
@@ -13,9 +13,7 @@
 <template>
   <div class="mx-auto mt-4 max-w-sm">
     <div class="mb-10 flex items-center justify-between">
-      <a target="_blank" href="https://productdevbook.com"
-        >productdevbook.com</a
-      >
+      
       <div class="flex space-x-2">
         <TemLanguageChange />
         <TemColorChange />
